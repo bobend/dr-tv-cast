d1a1df596b104e9509fddcdad836a33984545d76
Bo Bendtsen <bo.bendtsen@gmail.com>
2014-05-29 23:17:33 +0200

    Initial commit

---
+++ app/src/main/java/dr/dk/drtvcast/app/EpisodeDetailFragment.java
@@ -0,0 +1,104 @@
+package dr.dk.drtvcast.app;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import dr.dk.drtvcast.app.dummy.DummyContent;
+import dr.dk.drtvcast.app.mu.MuOnline;
+
+/**
+ * A fragment representing a single Episode detail screen.
+ * This fragment is either contained in a {@link EpisodeListActivity}
+ * in two-pane mode (on tablets) or a {@link EpisodeDetailActivity}
+ * on handsets.
+ */
+public class EpisodeDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private MuOnline.Item mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public EpisodeDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            String id = getArguments().getString(ARG_ITEM_ID);
+            mItem = MuOnline.LoadProgramCard(id);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_episode_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.title)).setText(mItem.Title);
+            ((TextView) rootView.findViewById(R.id.description)).setText(mItem.Description);
+
+            ImageView image = (ImageView) rootView.findViewById(R.id.imageView);
+            try {
+                URL thumb_url = new URL(mItem.PrimaryImageUri);
+                Drawable thumb = Drawable.createFromStream(thumb_url.openStream(), "src");
+                image.setImageDrawable(thumb);
+            } catch (MalformedURLException e) {
+            } catch (IOException e) {
+            }
+
+            Button button = (Button) rootView.findViewById(R.id.play);
+            button.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Intent player = new Intent(rootView.getContext(),videoPlayer.class);
+                    player.putExtra("assetUri", mItem.PrimaryAsset.Uri);
+                    startActivity(player);
+                }
+            });
+
+            //image.setImageURI(Uri.parse(mItem.PrimaryImageUri));}
+        }
+        /*
+        VideoView videoView = (VideoView) rootView.findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse("http://drod02c-vh.akamaihd.net/i/dk/clear/streaming/c4/5383e0d3a11f9d1560377bc4/Flodmonstre-IV--6-7-_8a67ef4654d645289d0b4b4db42c6c83_,1126,562,248,122,.mp4.csmil/master.m3u8"));
+        MediaController mc = new MediaController(rootView.getContext());
+        videoView.setMediaController(mc);
+        videoView.requestFocus();
+        videoView.start();*/
+        return rootView;
+    }
+}
---
+++ app/src/main/java/dr/dk/drtvcast/app/mu/MuOnline.java
@@ -0,0 +1,103 @@
+package dr.dk.drtvcast.app.mu;
+
+import android.net.Uri;
+import android.os.Debug;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import com.google.gson.*;
+
+/**
+ * Created by Bo on 29-05-2014.
+ */
+public class MuOnline {
+
+    public static FrontPage FRONT_PAGE;
+    static {
+
+    }
+    public static void LoadFrontPage(){
+        if (FRONT_PAGE != null)
+            return;
+        try {
+            URL api = new URL("http://www.dr.dk/mu-online/api/1.0/page/tv/front/");
+            BufferedReader in = new BufferedReader( new InputStreamReader(api.openStream()));
+            Gson gson = new GsonBuilder().create();
+            FRONT_PAGE= gson.fromJson(in, FrontPage.class);
+        } catch (MalformedURLException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+    }
+
+    public static Item LoadProgramCard(String slug){
+        Item res = null;
+        try {
+            URL api = new URL("http://www.dr.dk/mu-online/api/1.0/programcard/"+slug);
+            BufferedReader in = new BufferedReader( new InputStreamReader(api.openStream()));
+            Gson gson = new GsonBuilder().create();
+            res = gson.fromJson(in, Item.class);
+        } catch (MalformedURLException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+        return res;
+    }
+
+    public static String GetHLS(String assetUri){
+        try {
+            URL api = new URL(assetUri);
+            BufferedReader in = new BufferedReader( new InputStreamReader(api.openStream()));
+            Gson gson = new GsonBuilder().create();
+            Manifest manifest = gson.fromJson(in, Manifest.class);
+            for(int x=0; x<manifest.Links.size(); x++){
+                Link link = manifest.Links.get(x);
+                if (link.Target.equalsIgnoreCase("HLS"))
+                {
+                    return link.Uri;
+                }
+            }
+        } catch (MalformedURLException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+        return null;
+    }
+
+    public static class FrontPage {
+        public ItemList SelectedList;
+    }
+    public static class ItemList {
+        public List<Item> Items;
+    }
+
+    public static class Item {
+        public String Slug;
+        public String Title;
+        public String Description;
+        public String PrimaryImageUri;
+        public String toString() { return Title;}
+        public String id() { return Slug;}
+        public Asset PrimaryAsset;
+    }
+    public static class Asset {
+        public String Uri;
+    }
+    public static class Manifest {
+        public List<Link> Links ;
+    }
+    public static class Link {
+        public String Uri;
+        public String Target;
+    }
+}
